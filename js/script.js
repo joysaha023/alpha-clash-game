998ed38193b7628b0b7dc45d91ce628415901a62
Joy <github.joysaha@gmail.com>
2024-02-15 12:43:14 +0600

    function added on game

--- js/script.js
+++ js/script.js
@@ -8,10 +8,61 @@
 //     playground.classList.remove('hidden')
 // }
 
+function handleKeyboardUpEvent(event){
+    const playerPressed = event.key;
+    // console.log('player pressed',playerPressed);
+
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+
+    //get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const curretAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = curretAlphabet.toLocaleLowerCase();
+    // console.log(playerPressed, expectedAlphabet);
+
+    //check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log('You get a point');
+        
+        const currenScore = getTextElementvalueById('current-score');
+        const updatedScore = currenScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+        // const currentScoreElement = getTextElementvalueById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // const newScore = currentScore + 1;
+        // currentScoreElement.innerText = newScore;
+
+        removeBackgroundColorById(expectedAlphabet);
+        continuewGame();
+    }
+    else{
+        console.log('You missed. you lost a life');
+        const currentLife = getTextElementvalueById('current-life');
+        const updateLife = currentLife - 1;
+        setTextElementValueById('current-life', updateLife);
+
+        if(updateLife === 0){
+            gameOver();
+        }
+
+
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+        // const newLife = currentLife - 1;
+        // currentLifeElement.innerText = newLife;
+    }
+}
+document.addEventListener('keyup', handleKeyboardUpEvent)
+
 function continuewGame(){
     // generat a rendom alphabet
    const alphabet =  getARandomAlphabet();
-    console.log('your random alphabets:', alphabet);
+    // console.log('your random alphabets:', alphabet);
 
     //set a randomly alphabet to the screen
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -23,6 +74,23 @@ function continuewGame(){
 
 function play() {
     hideElementById('home-screen');
+    hideElementById('score');
     showElementById('play-ground');
+
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
     continuewGame();
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('score');
+
+    const lastScore = getTextElementvalueById('current-score');
+    setTextElementValueById('game-score', lastScore);
+
+    const currentAlpha = getElementTextById('current-alphabet');
+    // console.log(currentAlpha)
+    removeBackgroundColorById(currentAlpha);
 }
--- js/utility.js
+++ js/utility.js
@@ -13,6 +13,29 @@ function addBackgroundColorById(elementId){
     element.classList.add('bg-orange-400')
 }
 
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400')
+}
+
+function getTextElementvalueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
 function getARandomAlphabet(){
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
